ccb5fd2a3058823cb18879e996f46c0c1d510df3
colleg <colleg@cc.psu>
2022-11-08 17:58:09 +0300

    Расширил алфавит для генерации пароля

--- pr8_Karandashov/main.py
+++ pr8_Karandashov/main.py
@@ -1,5 +1,5 @@
 import random
 pas=''
 for x in range(11): #Количество символов (11)
-    pas = pas + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ')) #Символы, из которых будет составлен пароль
+    pas = pas + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZабвгдежзиклмнопрстуфхцшщьыъэюя')) #Символы, из которых будет составлен пароль
 print('Your password is: ', pas)
